4c876f74f12b1e5b7d67ca6a14bc599bf0b4cfa7
Kirstóf Umann <dkszelethus@gmail.com>
2021-11-04 16:00:45 +0100

    [clang-tidy] Fix a crash in modernize-loop-convert around conversion operators
    modernize-loop-convert checks and fixes when a loop that iterates over the
    elements of a container can be rewritten from a for(...; ...; ...) style into
    the "new" C++11 for-range format. For that, it needs to parse the elements of
    that loop, like its init-statement, such as ItType it = cont.begin().
    modernize-loop-convert checks whether the loop variable is initialized by a
    begin() member function.
    
    When an iterator is initialized with a conversion member function (e.g. for
    (const_iterator it = non_const_container.begin(); ...), attempts to retrieve the
    name of the initializer expression resulted in an assertion failure, as
    conversion member functions don't have a valid IdentifierInfo.
    
    I fixed this by making digThroughConstructors dig through conversion operators
    as well.
    
    Differential Revision: https://reviews.llvm.org/D113201

--- clang-tools-extra/test/clang-tidy/checkers/modernize-loop-convert-basic.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/modernize-loop-convert-basic.cpp
@@ -273,6 +273,16 @@ void f() {
   // CHECK-FIXES: for (int & It : Tt)
   // CHECK-FIXES-NEXT: printf("I found %d\n", It);
 
+  // Do not crash because of Qq.begin() converting. Q::converting_iterator
+  // converts with a conversion iterator, which has no name.
+  Q Qq;
+  for (Q::iterator It = Qq.begin(), E = Qq.end(); It != E; ++It) {
+    printf("I found %d\n", *It);
+  }
+  // CHECK-MESSAGES: :[[@LINE-3]]:3: warning: use range-based for loop instead
+  // CHECK-FIXES: for (int & It : Qq)
+  // CHECK-FIXES-NEXT: printf("I found %d\n", It);
+
   T *Pt;
   for (T::iterator It = Pt->begin(), E = Pt->end(); It != E; ++It) {
     printf("I found %d\n", *It);
