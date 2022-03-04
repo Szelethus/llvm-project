bc6c3ae79dd21549ecc67836ccd87e0050f97749
Kristóf Umann <dkszelethus@gmail.com>
2022-03-04 11:12:56 +0100

    new test cases

--- clang/test/Analysis/null-pointer-interference.c
+++ clang/test/Analysis/null-pointer-interference.c
@@ -1,7 +1,10 @@
 // RUN: %clang_analyze_cc1 -verify %s -analyzer-output=text\
-// RUN:   -analyzer-checker=core \
-// RUN:   -analyzer-checker=alpha.core.ReverseNull
+// RUN:   -analyzer-checker=core,apiModeling \
+// RUN:   -analyzer-checker=debug.ExprInspection \
+// RUN:   -analyzer-checker=alpha.core.ReverseNull \
+// RUN:   -analyzer-config apiModeling.StdCLibraryFunctions:ModelPOSIX=true
 
+void clang_analyzer_warnIfReached();
 //===----------------------------------------------------------------------===//
 // True positive test cases.
 //===----------------------------------------------------------------------===//
@@ -28,16 +31,40 @@ void tp2(int *p) {
     return;
 }
 
+long a64l(const char *str64);
+
+void tp3_posix_nonnull_constraint(const char *p) {
+  a64l(p);
+  // expected-note@-1{{Pointer assumed non-null here}}
+  // expected-note@-2{{Consider moving the condition here}}
+  if (p)
+    // expected-note@-1{{Pointer is unconditionally non-null here}}
+    // expected-warning@-2{{Pointer is unconditionally non-null here [alpha.core.ReverseNull]}}
+    return;
+}
+
+char* getenv (const char* name);
+
+void tp4_nonnull_constraint(const char *p) {
+  getenv(p);
+  // expected-note@-1{{Pointer assumed non-null here}}
+  // expected-note@-2{{Consider moving the condition here}}
+  if (p)
+    // expected-note@-1{{Pointer is unconditionally non-null here}}
+    // expected-warning@-2{{Pointer is unconditionally non-null here [alpha.core.ReverseNull]}}
+    return;
+}
+
 //===----------------------------------------------------------------------===//
 // True negative test cases.
 //===----------------------------------------------------------------------===//
 
-//void tn1_nested(int *p) {
-//  *p = 5;
-//}
-//
-//void tn1(int *p) {
-//  tn1_nested(p);
-//  if (p)
-//    return;
-//}
+void tn1_nested(int *p) {
+  *p = 5;
+}
+
+void tn1_constraint_in_nested_stackframe(int *p) {
+  tn1_nested(p);
+  if (p)
+    return;
+}
