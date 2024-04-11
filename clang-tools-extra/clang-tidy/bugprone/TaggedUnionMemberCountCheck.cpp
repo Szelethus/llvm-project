be3db43e0f4a207a1cf99fb77012240e6e70a4d0
Kristóf Umann <dkszelethus@gmail.com>
2024-04-11 14:15:51 +0200

    Some formatting
    Change-Id: I0f6f6f3c770af33cbb696edbacc7ffd8ac06c7cf

--- clang-tools-extra/clang-tidy/bugprone/TaggedUnionMemberCountCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/TaggedUnionMemberCountCheck.cpp
@@ -7,11 +7,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "TaggedUnionMemberCountCheck.h"
-#include "clang/AST/Expr.h"
 #include "clang/AST/ASTContext.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
-
+#include "clang/AST/Expr.h"
 #include "clang/AST/PrettyPrinter.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "llvm/Support/Casting.h"
 #include <iterator>
 #include <limits>
@@ -23,65 +22,92 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::bugprone {
 
 void TaggedUnionMemberCountCheck::registerMatchers(MatchFinder *Finder) {
-  Finder->addMatcher(recordDecl(allOf(isStruct(), has(fieldDecl(hasType(recordDecl(isUnion()).bind("union")))), has(fieldDecl(hasType(enumDecl().bind("tags")))))).bind("root"), this);
+  Finder->addMatcher(
+      recordDecl(
+          allOf(isStruct(),
+                has(fieldDecl(hasType(recordDecl(isUnion()).bind("union")))),
+                has(fieldDecl(hasType(enumDecl().bind("tags"))))))
+          .bind("root"),
+      this);
 }
 
-void TaggedUnionMemberCountCheck::check(const MatchFinder::MatchResult &Result) {
-  auto *root       = Result.Nodes.getNodeAs<RecordDecl>("root");
-  auto *unionMatch = Result.Nodes.getNodeAs<RecordDecl>("union");
-  auto *tagMatch   = Result.Nodes.getNodeAs<EnumDecl>("tags");
+void TaggedUnionMemberCountCheck::check(
+    const MatchFinder::MatchResult &Result) {
+  auto *Root = Result.Nodes.getNodeAs<RecordDecl>("root");
+  auto *UnionMatch = Result.Nodes.getNodeAs<RecordDecl>("union");
+  auto *TagMatch = Result.Nodes.getNodeAs<EnumDecl>("tags");
 
   // Early exit if there are multiple enums or unions in the struct
-  int64_t tags = 0;
-  int64_t unions = 0;
-  for (RecordDecl::field_iterator it = root->field_begin(); it != root->field_end(); it++) {
-    const FieldDecl *r = *it;
-    TypeSourceInfo *info = r->getTypeSourceInfo();
-    QualType qualtype = info->getType();
-    const Type *type = qualtype.getTypePtr();
-    if (type->isUnionType()) unions += 1;
-    else if (type->isEnumeralType()) tags += 1;
-    if (tags > 1 || unions > 1) return;
+  int64_t Tags = 0;
+  int64_t Unions = 0;
+  for (const FieldDecl *R : Root->fields()) {
+    TypeSourceInfo *Info = R->getTypeSourceInfo();
+    QualType Qualtype = Info->getType();
+    const Type *Type = Qualtype.getTypePtr();
+    if (Type->isUnionType())
+      Unions += 1;
+    else if (Type->isEnumeralType())
+      Tags += 1;
+    if (Tags > 1 || Unions > 1)
+      return;
   }
 
-  int64_t maxTagValue = std::numeric_limits<int64_t>::min();
-  int64_t minTagValue = std::numeric_limits<int64_t>::max();
-  int64_t counterEnumConstantCount = 0;
-  int64_t counterEnumConstantValue = 0;
-  int64_t firstCounterEnumConstantIndex = 0;
+  int64_t MaxTagValue = std::numeric_limits<int64_t>::min();
+  int64_t MinTagValue = std::numeric_limits<int64_t>::max();
+  int64_t CounterEnumConstantCount = 0;
+  int64_t CounterEnumConstantValue = 0;
+  int64_t FirstCounterEnumConstantIndex = 0;
 
-  for (EnumDecl::enumerator_iterator it = tagMatch->enumerator_begin(); it != tagMatch->enumerator_end(); it++) {
-    EnumConstantDecl *r = *it;
-	int64_t enumValue = r->getInitVal().getExtValue();
+  for (EnumDecl::enumerator_iterator It = TagMatch->enumerator_begin();
+       It != TagMatch->enumerator_end(); It++) {
+    EnumConstantDecl *R = *It;
+    int64_t EnumValue = R->getInitVal().getExtValue();
 
-    char countStr[] = "count";
-	const char *enumConstantIdentifier = r->getIdentifier()->getNameStart();
-	if (strlen(enumConstantIdentifier) >= strlen(countStr) && 0 == strcmp(enumConstantIdentifier + r->getIdentifier()->getLength() - strlen(countStr), countStr)) {
-		counterEnumConstantValue = enumValue;
-		if (counterEnumConstantCount == 0) {
-			firstCounterEnumConstantIndex = std::distance(tagMatch->enumerator_begin(), it);
-		}
-		counterEnumConstantCount += 1;
-	}
+    auto IsLikelyCountValue = [](EnumConstantDecl *R) -> bool {
+      char countStr[] = "count";
+      const char *EnumConstantIdentifier = R->getIdentifier()->getNameStart();
+      return strlen(EnumConstantIdentifier) >= strlen(countStr) &&
+             0 == strcmp(EnumConstantIdentifier +
+                             R->getIdentifier()->getLength() - strlen(countStr),
+                         countStr);
+    };
 
-	if (enumValue > maxTagValue) maxTagValue = enumValue;
-	if (enumValue < minTagValue) minTagValue = enumValue;
+    if (IsLikelyCountValue(R)) {
+      CounterEnumConstantValue = EnumValue;
+      if (CounterEnumConstantCount == 0) {
+        FirstCounterEnumConstantIndex =
+            std::distance(TagMatch->enumerator_begin(), It);
+      }
+      CounterEnumConstantCount += 1;
+    }
+
+    if (EnumValue > MaxTagValue)
+      MaxTagValue = EnumValue;
+    if (EnumValue < MinTagValue)
+      MinTagValue = EnumValue;
   }
 
-  int64_t enumCountFinal = maxTagValue - minTagValue + 1;
-  int64_t memberCount = std::distance(unionMatch->field_begin(), unionMatch->field_end());
-  int64_t enumConstantsCount = std::distance(tagMatch->enumerator_begin(), tagMatch->enumerator_end());
-  
-  if (memberCount > enumCountFinal) {
-    diag(root->getLocation(), "Tagged union has more data members than tags! Data members: %0 Tags: %1") << memberCount << enumCountFinal;
-  }
-  else if (memberCount < enumCountFinal) {
-    diag(root->getLocation(), "Tagged union has less data members than tags! Data members: %0 Tags: %1") << memberCount << enumCountFinal;
-  }
-  else if (counterEnumConstantCount == 1 && firstCounterEnumConstantIndex == enumConstantsCount - 1 && counterEnumConstantValue == maxTagValue) {
-    diag(root->getLocation(), "Tagged union has more data members than tags! Data members: %0 Tags: %1") << memberCount << enumCountFinal - counterEnumConstantCount;
-  }
+  int64_t EnumCountFinal = MaxTagValue - MinTagValue + 1;
+  int64_t MemberCount =
+      std::distance(UnionMatch->field_begin(), UnionMatch->field_end());
+  int64_t EnumConstantsCount =
+      std::distance(TagMatch->enumerator_begin(), TagMatch->enumerator_end());
 
+  if (MemberCount > EnumCountFinal) {
+    diag(Root->getLocation(), "Tagged union has more data members than tags! "
+                              "Data members: %0 Tags: %1")
+        << MemberCount << EnumCountFinal;
+  } else if (MemberCount < EnumCountFinal) {
+    diag(Root->getLocation(), "Tagged union has less data members than tags! "
+                              "Data members: %0 Tags: %1")
+        << MemberCount << EnumCountFinal;
+  } else if (CounterEnumConstantCount == 1 &&
+             FirstCounterEnumConstantIndex == EnumConstantsCount - 1 &&
+             CounterEnumConstantValue == MaxTagValue) {
+    diag(Root->getLocation(), "Tagged union has more data members than tags! "
+                              "Data members: %0 Tags: %1")
+        << MemberCount << EnumCountFinal - CounterEnumConstantCount;
+  }
 }
 
 } // namespace clang::tidy::bugprone
